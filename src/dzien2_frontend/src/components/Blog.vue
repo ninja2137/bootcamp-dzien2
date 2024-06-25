5d4d6bc64d7b6d7a7a4589c2cf87da29919b26dc
ninja2137 <t.kunicki@fedora.email>
2024-06-25 19:55:04 +0200

    dodano możliwość dodawania wpisów

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -3,6 +3,8 @@
         <button @click="pobierzWpisy">odśwież</button>
         siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
 
@@ -11,12 +13,16 @@ import { dzien2_backend } from 'declarations/dzien2_backend/index';
 export default {
 data(){
     return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
        async pobierzWpisy(){
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        },
+        async dodajWpisy(){
+            this.wpisy = await dzien2_backend.dodaj_wpis(this.nowyBlog);
         }
     }
 }
