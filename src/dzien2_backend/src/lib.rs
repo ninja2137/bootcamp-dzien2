8b47ec01b7a091243a17ca349abb8532e71fe8d1
ninja2137 <t.kunicki@fedora.email>
2024-06-25 19:11:51 +0200

    dodano sposób na wyświetlenie wpisów

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -4,7 +4,6 @@ thread_local! {
     static WPISY: RefCell<Vec<String>> = RefCell::default();
 }
 
-
 #[ic_cdk::query]
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
@@ -12,7 +11,14 @@ fn greet(name: String, last_name: i8) -> String {
 
 #[ic_cdk::update]
 fn dodaj_wpis(wpis: String) {
-   WPISY.with(|wpisy|{
-    wpisy.borrow_mut().push(wpis)
-   }) 
+    WPISY.with(|wpisy| {
+        wpisy.borrow_mut().push(wpis)
+    });
+}
+
+#[ic_cdk::query]
+fn odczytaj_wpisy() -> Vec<String> {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        wpisy.borrow().clone()
+    })
 }
